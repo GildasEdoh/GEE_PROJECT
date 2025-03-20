4492e261cfc40346663f82263da7e16bd2ab4f9a
GildasEdoh <edohgildas323@gmail.com>
2025-03-20 00:55:09 +0000

    backend

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,11 +7,21 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get('/test-db-connection', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Database connection successful!';
+    } catch (\Exception $e) {
+        return 'Could not connect to the database. Please check your configuration. Error: ' . $e->getMessage();
+    }
+});
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
