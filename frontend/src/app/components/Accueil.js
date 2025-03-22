5d910d8e8a9e04051bee4c8162ae4b67b21d11ef
Rahim10020 <rahialighi@gmail.com>
2025-03-22 12:01:38 +0000

    Ajustement de la page d'accueil

--- frontend/src/app/components/Accueil.js
+++ frontend/src/app/components/Accueil.js
@@ -6,14 +6,14 @@ import ActionButton from "./ActionButton"
 const Accueil = () => {
   return (
     <main className="p-4">
-      <div className="grid grid-cols-4 gap-4">
-        <InfoCard title="Nombre Total d’étudiants inscrits" value="-----" />
-        <InfoCard title="Nombre d’examens programmés" value="-----" />
+      <div className="flex justify-center gap-8">
+        <InfoCard title="Nombre Total d’étudiants inscrits" value="300" />
+        <InfoCard title="Nombre d’examens programmés" value="3" />
         <InfoCard title="Status des activités" value="-----" />
-        <InfoCard title="Nombre de sessions archivées" value="-----" />
+        <InfoCard title="Nombre de sessions archivées" value="5" />
       </div>
 
-      <div className="mt-6 flex space-x-4">
+      <div className="flex justify-center mt-8 space-x-8">
         <ActionButton label="Consulter les archives" onClick={() => alert("Archives ouvertes")} />
         <ActionButton label="Saisir une note" onClick={() => alert("Saisie de note")} />
         <ActionButton label="Nouvelle inscription" onClick={() => alert("Nouvelle inscription")} />
