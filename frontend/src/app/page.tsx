a0552f0a74f3768d34bde4606b86f62a861d6a18
Felix_Agbavo <agkofelix2022@gmail.com>
2025-04-10 18:26:43 +0000

    .

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -47,22 +47,29 @@
 //   );
 // }
 
+// "use client";
+// import Navbar from "./components/NavBar";
+// import SideBar from "./components/SideBar";
+// import Dashboard from "./components/Dashboard";
+// import { useState } from "react";
+
+// export default function Home() {
+//   const [selectedMenu, setSelectedMenu] = useState("accueil");
+
+//   return (
+//     <div className="flex h-screen">
+//       <SideBar setSelectedMenu={setSelectedMenu} />
+//       <div className="flex-1">
+//         <Navbar />
+//         <Dashboard selectedMenu={selectedMenu} />
+//       </div>
+//     </div>
+//   );
+// }
+
 "use client";
-import Navbar from "./components/NavBar";
-import SideBar from "./components/SideBar";
-import Dashboard from "./components/Dashboard";
-import { useState } from "react";
+import Statistique from './components/Statistique';
 
-export default function Home() {
-  const [selectedMenu, setSelectedMenu] = useState("accueil");
-
-  return (
-    <div className="flex h-screen">
-      <SideBar setSelectedMenu={setSelectedMenu} />
-      <div className="flex-1">
-        <Navbar />
-        <Dashboard selectedMenu={selectedMenu} />
-      </div>
-    </div>
-  );
+export default function Statistique() {
+  return <Statistique />;
 }
