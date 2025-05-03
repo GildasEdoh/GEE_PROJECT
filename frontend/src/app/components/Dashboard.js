b6ffa61026fb90bdf5c12562d217ef7478d2325c
GildasEdoh <edohgildas323@gmail.com>
2025-05-03 20:23:12 +0000

    maj

--- frontend/src/app/components/Dashboard.js
+++ frontend/src/app/components/Dashboard.js
@@ -4,33 +4,24 @@
 
 "use client";
 import Matieres from "./Matieres";
-<<<<<<< HEAD
 import Etudiants from "./Etudiants";
 import Accueil from "./Accueil";
 import MajNotes from "./MajNotes";
-=======
-import Etudiants from "./Etudiants"
-import Accueil from "./Accueil"
-import MajNotes from "./MajNotes"
 import Statistique from "./Statistique";
->>>>>>> 81c7cf033318ddf75373da768563169d82d20e84
 export default function Dashboard({ selectedMenu }) {
   const renderContent = () => {
     switch (selectedMenu) {
       case "Matières":
         return <Matieres />;
       case "Étudiants":
-<<<<<<< HEAD
         return <Etudiants />;
       case "Notes":
         return <MajNotes />;
-=======
         return <Etudiants/>
       case "Mise à jour Notes":
         return <MajNotes/>
       case "Statistiques":
         return <Statistique/>
->>>>>>> 81c7cf033318ddf75373da768563169d82d20e84
       default:
         return <Accueil />;
     }
